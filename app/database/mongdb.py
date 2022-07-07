f059523bea77c29d26da57d8c0f11aebb13e2b42
guanlaoby <guanlaoby@gmail.com>
2022-07-07 10:04:42 +0800

    learn mongodb

---
+++ app/database/mongdb.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+import time
+
+import pymongo
+start = time.time()
+client = pymongo.MongoClient("mongodb+srv://tas:ep0lVHTAYHXFOfIn@cluster0.lo0kf.mongodb.net/?retryWrites=true&w=majority")
+# use a database named "myDatabase"
+db = client.sample_training
+# FIND DOCUMENTS
+#
+# Now that we have data in Atlas, we can read it. To retrieve all of
+# the data in a collection, we call find() with an empty filter. 
+my_collection = db["companies"]
+result = my_collection.count_documents({})
+
+if result:    
+    print(result)
+    
+else:
+  print("No documents found.")
+
+print("\n")
+end = time.time()
+
+print("The time used to execute this is given below")
+print(end - start)
